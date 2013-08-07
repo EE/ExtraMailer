9c850e1386fff4c13fa071173c73d236c2b10c08
Jarek Rencz <jaroslaw@rencz.pl>
2013-08-07 16:06:47 +0200

    Revert "added ability to define multiple recipients with separate contexts"
    This reverts commit b9618dd3b0ef2f3a79d0abef72774b7ef22cc546.

--- Tests/ExtraMailerTest.php
+++ Tests/ExtraMailerTest.php
@@ -40,7 +40,7 @@ class ExtraMailerTest extends \PHPUnit_Framework_TestCase
             'demo' => file_get_contents(
                 __DIR__ . '/../Resources/views/Demo/sample.email.twig'
             ),
-            'demoTXT' => '{% block subject %}Sample subject for {{name}} in email sent by EEExtraMailer{% endblock %}{% block body_text %}Hi {{ name }}, This is sample email sent by EEExtraMailer{% endblock %}{% block body_html %}<p>Hi {{ name }}, This is sample email sent by EEExtraMailer</p>{% endblock %}'
+            'demoTXT' => '{% block subject %}Sample subject, email send by EEExtraMailer{% endblock %}{% block body_text %}This is sample email send by EEExtraMailer{% endblock %}'
         );
 
         $this->twig = $this->getMock('Twig_Environment', null, array(new \Twig_Loader_Array($this->templates)));
@@ -50,7 +50,7 @@ class ExtraMailerTest extends \PHPUnit_Framework_TestCase
     public function testSendMessage()
     {
         $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-        $this->swift->expects($this->once())->method('send')->will($this->returnValue(1));
+        $this->swift->expects($this->once())->method('send')->will($this->returnValue(0));
         $result = $extraMailer->sendMessage(
             array('recipient@example.com' => 'Recipient Name'),
             'demo',
@@ -58,13 +58,13 @@ class ExtraMailerTest extends \PHPUnit_Framework_TestCase
             array('attachments' => array(__DIR__ . '/../Resources/views/Demo/sample.email.twig'))
         );
 
-        $this->assertEquals(1, $result);
+        $this->assertTrue($result);
     }
 
     public function testSendMessageTxt()
     {
         $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-        $this->swift->expects($this->once())->method('send')->will($this->returnValue(1));
+        $this->swift->expects($this->once())->method('send')->will($this->returnValue(0));
         $result = $extraMailer->sendMessage(
             array('recipient@example.com' => 'Recipient Name'),
             'demoTXT',
@@ -72,108 +72,6 @@ class ExtraMailerTest extends \PHPUnit_Framework_TestCase
             array('attachments' => array(__DIR__ . '/../Resources/views/Demo/sample.email.twig'))
         );
 
-        $this->assertEquals(1, $result);
-    }
-
-
-    public function testSendMessageToManyWithCommonContext()
-    {
-        $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-
-        $recipients = array(
-            'somename@example.com' => 'Somename',
-            'someothername@example.com' => 'Someothername'
-        );
-
-        $this->swift
-            ->expects($this->once())
-            ->method('send')
-            ->will($this->returnValue(count($recipients)));
-
-
-        $result = $extraMailer->sendMessage(
-            $recipients,
-            'demo',
-            array('name' => 'lol'),
-            array('attachments' => array(__DIR__ . '/../Resources/views/Demo/sample.email.twig'))
-        );
-
-        $this->assertEquals(count($recipients), $result);
-    }
-
-    public function testPrepareMessagesToManyWithSeparateContexts()
-    {
-        $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-
-        $recipients = array(
-            'somename@example.com' => 'Somename',
-            'someothername@example.com' => 'Someothername'
-        );
-        $contexts = array(
-            array('name' => 'Example Name 1'),
-            array('name' => 'Example Name 2')
-        );
-
-        $messagesArray = $extraMailer->composeMessages(
-            $recipients,
-            'demoTXT',
-            $contexts
-        );
-
-        $expectedSubjectsArray = array(
-            'Sample subject for Example Name 1 in email sent by EEExtraMailer',
-            'Sample subject for Example Name 2 in email sent by EEExtraMailer'
-        );
-
-        for ($i = 0; $i< count($contexts); $i++) {
-            $this->assertEquals(
-                $expectedSubjectsArray[$i],
-                $messagesArray[$i]->getSubject()
-            );
-        }
-
-    }
-
-    public function testGetRenderedSubject()
-    {
-        $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-
-        $template = $this->twig->loadTemplate('demoTXT');
-
-
-        $subject = $extraMailer->getRenderedSubject($template, array('name' => 'abcdef'));
-
-        $expected = 'Sample subject for abcdef in email sent by EEExtraMailer';
-
-        $this->assertEquals($expected, $subject);
-    }
-
-
-    public function testGetRenderedBodyText()
-    {
-        $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-
-        $template = $this->twig->loadTemplate('demoTXT');
-
-
-        $subject = $extraMailer->getRenderedBodyText($template, array('name' => 'abcdef'));
-
-        $expected = 'Hi abcdef, This is sample email sent by EEExtraMailer';
-
-        $this->assertEquals($expected, $subject);
-    }
-
-    public function testGetRenderedBodyHtml()
-    {
-        $extraMailer = new ExtraMailer($this->container, $this->swift, $this->twig);
-
-        $template = $this->twig->loadTemplate('demoTXT');
-
-
-        $subject = $extraMailer->getRenderedBodyHtml($template, array('name' => 'abcdef'));
-
-        $expected = '<p>Hi abcdef, This is sample email sent by EEExtraMailer</p>';
-
-        $this->assertEquals($expected, $subject);
+        $this->assertTrue($result);
     }
 }
